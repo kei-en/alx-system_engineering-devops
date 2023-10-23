133e6344db582dfc8f24ab06c0e4cced352706c6
kei-en <njuguna0karanja@gmail.com>
2023-10-23 19:20:42 +0300

    Revert to prev code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,21 +4,11 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    user = '{}users/{}'.format(url, sys.argv[1])
-    res = requests.get(user)
-    jso_n = res.json()
-    print("Employee {} is done with tasks".format(jso_n.get('name')), end="")
-
-    todos = '{}todos?userId={}'.format(url, sys.argv[1])
-    res = requests.get(todos)
-    tasks = res.json()
-    tsk = []
-    for task in tasks:
-        if task.get('completed') is True:
-            tsk.append(task)
-
-    print("({}/{}):".format(len(tsk), len(tasks)))
-    for task in tsk:
-        print("\t {}".format(task.get("title")))
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
